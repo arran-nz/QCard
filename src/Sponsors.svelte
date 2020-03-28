ac06affcf5bf32b710c8c51074e691316972122d
Arran <code@arran.nz>
2020-03-28 20:24:15 +0100

    Refine Sponsors component

--- src/Sponsors.svelte
+++ src/Sponsors.svelte
@@ -3,7 +3,7 @@
     label {
         margin: 2em;
         color: #606c76;
-        font-size: 1.5em;
+        font-size: 1.2em;
         font-weight: 300;
         text-align: center;
     }
@@ -43,11 +43,10 @@
 
 <article>    
         <hr>
-   
         <label>
             <q>We don't track you</q>
             <strong>
-                <a href="https://simpleanalytics.com?ref=qcard.now.sh" alt="Simple Anayltics Main Page">
+                <a href="https://simpleanalytics.com?ref=qcard.now.sh" alt="Simple Anayltics Main Page" target="_blank">
                     <svg class="simple" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
                         <rect x="20" y="53" width="10" height="30"></rect>
                         <rect x="44" y="35" width="10" height="48"></rect>
@@ -59,8 +58,7 @@
         </label>
         
         <label>
-            Hosted with:
-            <a href="/" alt="Zeit">
+            <a href="https://zeit.co" alt="Zeit Main Page" target="_blank">
                 <svg width="94" height="35" viewBox="0 0 94 20"><g stroke="none" stroke-width="1" fill="var(--geist-foreground)" fill-rule="evenodd"><polygon points="11.0541053 0 22.1082105 19.6826969 0 19.6826969"></polygon><path d="M35.046 15.078h8.357v-1.456H37.13l6.171-8.84v-1.27h-8.153v1.455h6.069l-6.171 8.84v1.27zm17.104 0h7.5v-1.456h-5.804V9.91h5.047V8.454h-5.047V4.967h5.803V3.511h-7.5v11.567zm16.531 0h7.316v-1.456h-2.8V4.967h2.8V3.511h-7.316v1.456h2.82v8.655h-2.82v1.456zm19.352 0h1.696V4.968h3.924V3.51H84.15v1.456h3.883v10.11z"></path></g></svg>
             </a>
         </label>
