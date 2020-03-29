2f2ce49abab79e76481fadfc143ea3b853af5594
Arran <code@arran.nz>
2020-03-30 01:16:29 +0200

    Structured ContactCard, Vertical contact methods

--- src/ContactCard.svelte
+++ src/ContactCard.svelte
@@ -1,29 +1,62 @@
 <style>
 	#card {
-		padding: 1em;
+
+		display: flex;
+		flex-direction: column;
+
+		padding: 3vw;
+		
 		border-radius: 5px;
 
 		-webkit-box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.25);
 		-moz-box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.25);
 		box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.25);
 	}
+	h1, h2 {
+		text-align: left;
+
+	}
 
 	h1 {
-		text-align: center;
 		margin: 0;
-		font-size: 3em;
+		font-size: 2.5em;
 		line-height: 0.9em;
 
 	}
 
 	h2 {
-		text-align: center;
+		font-size: 1.2em;
 		margin: 0;
 		color: #606c76;
 	}
 
+	hr {
+		border: 1px solid whitesmoke;
+		margin: 2vh 0;
+	}
+
+
+	@media all and (min-width: 800px) {
+		#card {
+			padding: 20px;
+		}
+
+		h1 {
+			font-size: 3em;
+		}
+
+		h2 {
+			font-size: 1.5em;
+		}
+
+		hr {
+			margin: 20px 0;
+		}
+	}
+
+
 	header {
-		margin-bottom: 0.5em;
+		margin-bottom: 10px;
 	}
 
 	p {
@@ -32,25 +65,16 @@
 
 	#comment {
 		padding: 5px 0px;
-		border-left: 5px solid whitesmoke;
+		border-left: 7px solid whitesmoke;
 	}
 
 	#comment p {
 		margin: 5px 10px;
 	}
 
-	hr {
-		border: 1px solid whitesmoke;
-		margin: 1em 0;
-	}
 
-	.contact-info {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		justify-content: space-evenly;
-	}
 
+	/* Contact Copying */
 	ul.contact-copy {
 		margin: 1em;
 		list-style-type: none;
@@ -63,7 +87,7 @@
 		letter-spacing: 2px;
 
 		margin: 0.5em 0;
-		padding: 0.5em; 
+		padding: 0.5em;
 
 		background-color: whitesmoke;
 		border-radius: 5px;
@@ -79,18 +103,8 @@
 	a {
 		font-size: 1.2em;
 		color:inherit;
-	}
+  	}
 
-	.info-slot {
-		margin: 15px;
-	}
-
-	.info-slot svg{
-		min-width: 20px;
-		max-width: 45px;
-		width: 8vw;
-		transition-duration: 0.3s;
-	}
 
 	a:hover svg{
 		fill: #ff3e00;
@@ -98,7 +112,6 @@
 	}
 
 	div.button-container {
-		width: 100;
 		display: flex;
 		justify-content: flex-end;
 
@@ -144,12 +157,47 @@
 	.create-footer-link strong {
 		color: #ff3e00;
 		font-weight: 600;
-	}	
+	}
+	
+	/*Contact Info*/
+
+	.contact-meta {
+		display:flex;
+		flex-direction: column;
+		justify-content: space-evenly
+
+	}
+
+	.contact-info-container {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+
+	}
+
+	.contact-methods {
+		border-radius: 5px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+	}
+
+	.contact-method-item {
+		margin: 0.5em;
+	}
+
+	.contact-method-item svg{
+		min-width: 25px;
+		max-width: 35px;
+		width: 8vw;
+		transition-duration: 0.3s;
+	}
 
 </style>
 
 <script>
 	import { fly } from 'svelte/transition';
+	import { backOut } from 'svelte/easing';
 
 	import QRCode from './QRCode.svelte';
 	import { generateVCardString } from './plugins/vcard.js';
@@ -164,7 +212,6 @@
 	export let loadedExternalVCard;
 
 	let showRawData = false;
-	let showCreateLink = false;
 
 	let vCardObj = {};
 	let vCardString;
@@ -181,7 +228,7 @@
 		setVCardProperty("tel", phone);
 		setVCardProperty("url", website);
 		setVCardProperty("note", comment);
-		
+
 		vCardString = generateVCardString(
 			vCardObj,
 			false
@@ -195,7 +242,7 @@
 			{
 				'value': value,
 			}
-		]		
+		]
 	};
 
 	function getSelfLink(){
@@ -219,7 +266,7 @@
 		downloadElement.click();
 		document.body.removeChild(downloadElement);
 	}
-	
+
 	function ShareQCard(){
 		if (navigator.share) {
 			navigator.share({
@@ -234,12 +281,15 @@
 
 </script>
 
-<article in:fly="{{ x: -100, duration: 600 }}" on:introend="{() => showCreateLink = true}">
+<article in:fly="{{ x: -100, duration: 600, easing: backOut }}">
 <div id="card">
   	{#if selfLink && !showRawData}
+
+	<div class="qrcode-container">
 		<a href={selfLink} alt="QRCard URL">
 			<QRCode dataToEncode = {selfLink}/>
 		</a>
+	</div>
 	{:else}
 		<ul class="contact-copy">
 
@@ -249,7 +299,7 @@
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="copy"><rect width="24" height="24" opacity="0"/><path d="M18 9h-3V5.67A2.68 2.68 0 0 0 12.33 3H5.67A2.68 2.68 0 0 0 3 5.67v6.66A2.68 2.68 0 0 0 5.67 15H9v3a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3v-6a3 3 0 0 0-3-3zm-9 3v1H5.67a.67.67 0 0 1-.67-.67V5.67A.67.67 0 0 1 5.67 5h6.66a.67.67 0 0 1 .67.67V9h-1a3 3 0 0 0-3 3z"/></g></g></svg>
 			</li>
 			{/if}
-			
+
 			{#if email}
 			<li>
 				{email}
@@ -275,89 +325,98 @@
 
 	<hr>
 
-	<header>
-		<h1>
-			{name}
-		</h1>
+	<!-- BEGIN-->
 
-		{#if title}
-		<h2>{title}</h2>
-		{/if}
+	<div class="contact-info-container">
 
-	</header>
+		<div class="contact-meta">
 
-	{#if email || phone || website}
-	<div class="contact-info">
-		{#if email}
-		<div class="info-slot">
-			<a href="mailto:{email}" alt="{email}" target="_blank">
+			<header>
+				<h1>
+					{name}
+				</h1>
+				{#if title}
+				<h2>{title}</h2>
+				{/if}
+			</header>
 
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-					<g data-name="Layer 2">
-					<g data-name="email">
-						<rect width="24" height="24" opacity="0" />
-						<path
-						d="M19 4H5a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a3
-						3 0 0 0-3-3zm0 2l-6.5 4.47a1 1 0 0 1-1 0L5 6z" />
-					</g>
-					</g>
-				</svg>
 
-			</a>
+			{#if comment}
+			<div id="comment">
+				<p>{comment}</p>
+			</div>
+			{/if}
 		</div>
-		{/if}
 
-		{#if phone}
-		<div class="info-slot">
-			<a href="tel:{phone}" alt={phone}>
 
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-					<g data-name="Layer 2">
-						<g data-name="phone-call">
-							<rect width="24" height="24" opacity="0" />
-							<path d="M13 8a3 3 0 0 1 3 3 1 1 0 0 0 2 0 5 5 0 0 0-5-5 1 1 0 0 0 0 2z" />
-							<path d="M13 4a7 7 0 0 1 7 7 1 1 0 0 0 2 0 9 9 0 0 0-9-9 1 1 0 0 0 0 2z" />
-							<path
-								d="M21.75 15.91a1 1 0 0 0-.72-.65l-6-1.37a1 1 0 0 0-.92.26c-.14.13-.15.14-.8 1.38a9.91 9.91 0 0 1-4.87-4.89C9.71 10 9.72 10 9.85 9.85a1 1 0 0 0 .26-.92L8.74 3a1 1 0 0 0-.65-.72 3.79 3.79 0 0 0-.72-.18A3.94 3.94 0 0 0 6.6 2 4.6 4.6 0 0 0 2 6.6 15.42 15.42 0 0 0 17.4 22a4.6 4.6 0 0 0 4.6-4.6 4.77 4.77 0 0 0-.06-.76 4.34 4.34 0 0 0-.19-.73z" />
-						</g>
-					</g>
-				</svg>
+		{#if email || phone || website}
+		<div class="contact-methods">
+			{#if email}
+			<div class="contact-method-item">
+				<a href="mailto:{email}" alt="{email}" target="_blank">
 
-			</a>
-		</div>
-		{/if}
-
-		{#if website}
-		<div class="info-slot">
-			<a href={website} target="_blank" alt="Website provided by {name}">
-
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-					<g data-name="Layer 2">
-						<g data-name="external-link">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+						<g data-name="Layer 2">
+						<g data-name="email">
 							<rect width="24" height="24" opacity="0" />
 							<path
-								d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z" />
-							<path
-								d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z" />
+							d="M19 4H5a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a3
+							3 0 0 0-3-3zm0 2l-6.5 4.47a1 1 0 0 1-1 0L5 6z" />
 						</g>
-					</g>
-				</svg>
+						</g>
+					</svg>
 
-			</a>
+				</a>
+			</div>
+			{/if}
+
+			{#if phone}
+			<div class="contact-method-item">
+				<a href="tel:{phone}" alt={phone}>
+
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+						<g data-name="Layer 2">
+							<g data-name="phone-call">
+								<rect width="24" height="24" opacity="0" />
+								<path d="M13 8a3 3 0 0 1 3 3 1 1 0 0 0 2 0 5 5 0 0 0-5-5 1 1 0 0 0 0 2z" />
+								<path d="M13 4a7 7 0 0 1 7 7 1 1 0 0 0 2 0 9 9 0 0 0-9-9 1 1 0 0 0 0 2z" />
+								<path
+									d="M21.75 15.91a1 1 0 0 0-.72-.65l-6-1.37a1 1 0 0 0-.92.26c-.14.13-.15.14-.8 1.38a9.91 9.91 0 0 1-4.87-4.89C9.71 10 9.72 10 9.85 9.85a1 1 0 0 0 .26-.92L8.74 3a1 1 0 0 0-.65-.72 3.79 3.79 0 0 0-.72-.18A3.94 3.94 0 0 0 6.6 2 4.6 4.6 0 0 0 2 6.6 15.42 15.42 0 0 0 17.4 22a4.6 4.6 0 0 0 4.6-4.6 4.77 4.77 0 0 0-.06-.76 4.34 4.34 0 0 0-.19-.73z" />
+							</g>
+						</g>
+					</svg>
+
+				</a>
+			</div>
+			{/if}
+
+			{#if website}
+			<div class="contact-method-item">
+				<a href={website} target="_blank" alt="Website provided by {name}">
+
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+						<g data-name="Layer 2">
+							<g data-name="external-link">
+								<rect width="24" height="24" opacity="0" />
+								<path
+									d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z" />
+								<path
+									d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z" />
+							</g>
+						</g>
+					</svg>
+
+				</a>
+			</div>
+			{/if}
 		</div>
 		{/if}
+
 	</div>
-	{/if}
 
 
+	<!-- STOP-->
 
-  	{#if comment}
-  	<div id="comment">
-    	<p>{comment}</p>
-  	</div>
-	{/if}
-
-	
 	<hr>
 
 	<div class="button-container">
@@ -377,7 +436,7 @@
 		{/if}
 
 		<button on:click={DownloadVCard} class="download" alt="Download the VCard">
-			
+
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 				<g data-name="Layer 2">
 					<g data-name="download">
@@ -397,8 +456,8 @@
 
 </div>
 
-	{#if loadedExternalVCard && showCreateLink}
-		<div class="create-footer-link" in:fly="{{ y: -20, duration: 600, delay:1000 }}">
+	{#if loadedExternalVCard}
+		<div class="create-footer-link" in:fly="{{ y: -20, duration: 600, delay:800, easing: backOut}}">
 			<a href={baseUrl}>Create your own <strong>QCard</strong></a>
 		</div>
 	{/if}
