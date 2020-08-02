180a8cbe3eb0f4ae23a8998007ab57ce98f935f8
Arran <code@arran.nz>
2020-08-02 17:34:38 +0200

    Clean up Sponsors

--- src/Sponsors.svelte
+++ src/Sponsors.svelte
@@ -4,8 +4,7 @@
         margin: 2em;
         color: #606c76;
         font-size: 1.2em;
-        font-weight: 300;
-        text-align: center;
+        font-weight: 500;
     }
 
     q {
@@ -26,18 +25,14 @@
         cursor: pointer;    
     }
 
-    svg.simple {
+    svg {
         box-sizing: border-box;
-        width: 30px;
-        height: 30px;
+        width: 24px;
+        height: 24px;
         fill: #ff4f64;        
-		}
-
-    svg.git {
-        width: 30px;
-        height: 30px;
-        fill: #ff3e00;
+        margin: 0px 2px;
     }
+
 
     hr {
         border: 1px solid whitesmoke;
@@ -50,29 +45,20 @@
 <article>    
 	<hr>
 	    <label>
-		    <svg class="git" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><rect width="24" height="24" opacity="0"/><path d="M16.24 22a1 1 0 0 1-1-1v-2.6a2.15 2.15 0 0 0-.54-1.66 1 1 0 0 1 .61-1.67C17.75 14.78 20 14 20 9.77a4 4 0 0 0-.67-2.22 2.75 2.75 0 0 1-.41-2.06 3.71 3.71 0 0 0 0-1.41 7.65 7.65 0 0 0-2.09 1.09 1 1 0 0 1-.84.15 10.15 10.15 0 0 0-5.52 0 1 1 0 0 1-.84-.15 7.4 7.4 0 0 0-2.11-1.09 3.52 3.52 0 0 0 0 1.41 2.84 2.84 0 0 1-.43 2.08 4.07 4.07 0 0 0-.67 2.23c0 3.89 1.88 4.93 4.7 5.29a1 1 0 0 1 .82.66 1 1 0 0 1-.21 1 2.06 2.06 0 0 0-.55 1.56V21a1 1 0 0 1-2 0v-.57a6 6 0 0 1-5.27-2.09 3.9 3.9 0 0 0-1.16-.88 1 1 0 1 1 .5-1.94 4.93 4.93 0 0 1 2 1.36c1 1 2 1.88 3.9 1.52a3.89 3.89 0 0 1 .23-1.58c-2.06-.52-5-2-5-7a6 6 0 0 1 1-3.33.85.85 0 0 0 .13-.62 5.69 5.69 0 0 1 .33-3.21 1 1 0 0 1 .63-.57c.34-.1 1.56-.3 3.87 1.2a12.16 12.16 0 0 1 5.69 0c2.31-1.5 3.53-1.31 3.86-1.2a1 1 0 0 1 .63.57 5.71 5.71 0 0 1 .33 3.22.75.75 0 0 0 .11.57 6 6 0 0 1 1 3.34c0 5.07-2.92 6.54-5 7a4.28 4.28 0 0 1 .22 1.67V21a1 1 0 0 1-.94 1z"/></g></svg>
-		    <a href="https://github.com/Azza292/QCard" alt="QCard Github">            
+            <a href="https://github.com/Azza292/QCard" alt="QCard Github" target="_blank">            
+                <svg class="git" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><rect width="24" height="24" opacity="0"/><path d="M16.24 22a1 1 0 0 1-1-1v-2.6a2.15 2.15 0 0 0-.54-1.66 1 1 0 0 1 .61-1.67C17.75 14.78 20 14 20 9.77a4 4 0 0 0-.67-2.22 2.75 2.75 0 0 1-.41-2.06 3.71 3.71 0 0 0 0-1.41 7.65 7.65 0 0 0-2.09 1.09 1 1 0 0 1-.84.15 10.15 10.15 0 0 0-5.52 0 1 1 0 0 1-.84-.15 7.4 7.4 0 0 0-2.11-1.09 3.52 3.52 0 0 0 0 1.41 2.84 2.84 0 0 1-.43 2.08 4.07 4.07 0 0 0-.67 2.23c0 3.89 1.88 4.93 4.7 5.29a1 1 0 0 1 .82.66 1 1 0 0 1-.21 1 2.06 2.06 0 0 0-.55 1.56V21a1 1 0 0 1-2 0v-.57a6 6 0 0 1-5.27-2.09 3.9 3.9 0 0 0-1.16-.88 1 1 0 1 1 .5-1.94 4.93 4.93 0 0 1 2 1.36c1 1 2 1.88 3.9 1.52a3.89 3.89 0 0 1 .23-1.58c-2.06-.52-5-2-5-7a6 6 0 0 1 1-3.33.85.85 0 0 0 .13-.62 5.69 5.69 0 0 1 .33-3.21 1 1 0 0 1 .63-.57c.34-.1 1.56-.3 3.87 1.2a12.16 12.16 0 0 1 5.69 0c2.31-1.5 3.53-1.31 3.86-1.2a1 1 0 0 1 .63.57 5.71 5.71 0 0 1 .33 3.22.75.75 0 0 0 .11.57 6 6 0 0 1 1 3.34c0 5.07-2.92 6.54-5 7a4.28 4.28 0 0 1 .22 1.67V21a1 1 0 0 1-.94 1z"/></g></svg>
                 Source
             </a>
 		</label>
 
         <label>
-            <q>We don't track you</q>
-            <strong>
-                <a href="https://simpleanalytics.com?ref=qcard.now.sh" alt="Simple Anayltics Main Page" target="_blank">
-                    <svg class="simple" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
-                        <rect x="20" y="53" width="10" height="30"></rect>
-                        <rect x="44" y="35" width="10" height="48"></rect>
-                        <rect x="68" y="18" width="10" height="65"></rect>
-                    </svg>
-                    Simple Analytics
-                </a>
-            </strong>
-        </label>
-        
-        <label>
-            <a href="https://zeit.co" alt="Zeit Main Page" target="_blank">
-                <svg width="94" height="35" viewBox="0 0 94 20"><g stroke="none" stroke-width="1" fill="var(--geist-foreground)" fill-rule="evenodd"><polygon points="11.0541053 0 22.1082105 19.6826969 0 19.6826969"></polygon><path d="M35.046 15.078h8.357v-1.456H37.13l6.171-8.84v-1.27h-8.153v1.455h6.069l-6.171 8.84v1.27zm17.104 0h7.5v-1.456h-5.804V9.91h5.047V8.454h-5.047V4.967h5.803V3.511h-7.5v11.567zm16.531 0h7.316v-1.456h-2.8V4.967h2.8V3.511h-7.316v1.456h2.82v8.655h-2.82v1.456zm19.352 0h1.696V4.968h3.924V3.51H84.15v1.456h3.883v10.11z"></path></g></svg>
+            <a href="https://simpleanalytics.com?ref=qcard.now.sh" alt="Simple Anayltics Main Page" target="_blank">
+                <svg class="simple" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
+                    <rect x="20" y="53" width="10" height="30"></rect>
+                    <rect x="44" y="35" width="10" height="48"></rect>
+                    <rect x="68" y="18" width="10" height="65"></rect>
+                </svg>
+                Simple Analytics
             </a>
         </label>
 
