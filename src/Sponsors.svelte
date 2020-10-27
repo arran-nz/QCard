dfb0ffc28f2a11700374b361370ec11d08c59772
Arran <code@arran.nz>
2020-10-27 14:53:51 +0100

    Update Sponsors - Display Simple Analytics counter, add git logo

--- src/Sponsors.svelte
+++ src/Sponsors.svelte
@@ -1,22 +1,30 @@
 <style>
 
+    .container {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        margin: 0 auto;
+    }
+
+    .container div {
+        margin: 1em 2em;
+        align-self: center;
+    }
+
     label {
-        margin: 2em;
+        margin: 0;
         color: #606c76;
-        font-size: 1.2em;
+        font-size: 1em;
         font-weight: 500;
     }
 
-    q {
-        font-style: italic;
-        quotes: "“" "”" "" "’";
+    .disclaimer {
+        margin: 0.1em 0 ;
+        font-size: 0.7em;
+        text-align: center;
     }
-    q:before {
-        content: open-quote;
-    }
-    q:after {
-        content: close-quote;
-    }
+
 
     a {
         justify-content: center;
@@ -36,30 +44,44 @@
 
     hr {
         border: 1px solid whitesmoke;
-        margin: 2em;
+        margin:  1em auto;
     }
 
+    .icon {
+        height:1em;
+        margin: 0 0.3em;
+        align-self: center;
+    }
+
+    @media all and (max-width: 600px) {
+        .container {
+            flex-direction: column;
+        }
+    }
 
 </style>
 
 <article>    
-	<hr>
-	    <label>
-            <a href="https://github.com/Azza292/QCard" alt="QCard Github" target="_blank">            
-                <svg class="git" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><rect width="24" height="24" opacity="0"/><path d="M16.24 22a1 1 0 0 1-1-1v-2.6a2.15 2.15 0 0 0-.54-1.66 1 1 0 0 1 .61-1.67C17.75 14.78 20 14 20 9.77a4 4 0 0 0-.67-2.22 2.75 2.75 0 0 1-.41-2.06 3.71 3.71 0 0 0 0-1.41 7.65 7.65 0 0 0-2.09 1.09 1 1 0 0 1-.84.15 10.15 10.15 0 0 0-5.52 0 1 1 0 0 1-.84-.15 7.4 7.4 0 0 0-2.11-1.09 3.52 3.52 0 0 0 0 1.41 2.84 2.84 0 0 1-.43 2.08 4.07 4.07 0 0 0-.67 2.23c0 3.89 1.88 4.93 4.7 5.29a1 1 0 0 1 .82.66 1 1 0 0 1-.21 1 2.06 2.06 0 0 0-.55 1.56V21a1 1 0 0 1-2 0v-.57a6 6 0 0 1-5.27-2.09 3.9 3.9 0 0 0-1.16-.88 1 1 0 1 1 .5-1.94 4.93 4.93 0 0 1 2 1.36c1 1 2 1.88 3.9 1.52a3.89 3.89 0 0 1 .23-1.58c-2.06-.52-5-2-5-7a6 6 0 0 1 1-3.33.85.85 0 0 0 .13-.62 5.69 5.69 0 0 1 .33-3.21 1 1 0 0 1 .63-.57c.34-.1 1.56-.3 3.87 1.2a12.16 12.16 0 0 1 5.69 0c2.31-1.5 3.53-1.31 3.86-1.2a1 1 0 0 1 .63.57 5.71 5.71 0 0 1 .33 3.22.75.75 0 0 0 .11.57 6 6 0 0 1 1 3.34c0 5.07-2.92 6.54-5 7a4.28 4.28 0 0 1 .22 1.67V21a1 1 0 0 1-.94 1z"/></g></svg>
-                Source
-            </a>
-		</label>
+    <hr>
+    <div class="container">
+    
+        <div id="sa">
+            <a href="https://simpleanalytics.com/qcard.link?utm_source=qcard.link&utm_content=badge" referrerpolicy="origin" target="_blank"><img class="simpleanalytics" src="https://simpleanalyticsbadge.com/qcard.link?background=transparent&text=606c76" loading="lazy" referrerpolicy="no-referrer" crossorigin="anonymous" /></a>
+            <label class="disclaimer">This website respects <abbr title="Do Not Track">DNT</abbr> requests</label>
+        </div>
 
-        <label>
-            <a href="https://simpleanalytics.com?ref=qcard.link" alt="Simple Anayltics Main Page" target="_blank">
-                <svg class="simple" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
-                    <rect x="20" y="53" width="10" height="30"></rect>
-                    <rect x="44" y="35" width="10" height="48"></rect>
-                    <rect x="68" y="18" width="10" height="65"></rect>
-                </svg>
-                Simple Analytics
-            </a>
-        </label>
+        <div id="source-code">
+            <label>
+                <a href="https://codeberg.org/arran/qcard" alt="QCard Source Code" target="_blank">            
+                    <img loading="lazy" class="icon" src="/icons/git.svg" alt="Copy Icon"/>
+                    <label>Source Code</label>
+                </a>
+            </label>
+            <label class="disclaimer"><abbr title="Free (Libre) Open Source Software">FLOSS</abbr> Software - Licensed Under GPLv3</label>
+        </div>
+
+    </div>
+
+
 
 </article>
